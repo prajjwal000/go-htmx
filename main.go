8e189596fce1d96acb47bf762cfeb590babc46e2
Prajjwal <prajjwalverma74@gmail.com>
2024-09-23 01:03:17 +0530

    routes life/view life/create added

--- main.go
+++ main.go
@@ -10,13 +10,38 @@ const (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello on home!!"))
+}
+
+func view(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("View your life flash before eyes"))
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Try again in next life", http.StatusMethodNotAllowed)
+
+		return
+	}
+
+	w.Write([]byte("Create your destiny"))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/life/view", view)
+	mux.HandleFunc("/life/create", create)
 
 	log.Println("Starting server on " + port)
 	err := http.ListenAndServe(port, mux)
