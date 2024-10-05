42e96875402d61bedc52b8973ab85ca72a9d6f59
Prajjwal <prajjwalverma74@gmail.com>
2024-10-05 23:46:35 +0530

    command line flags for quality of life

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
-const (
-	port = ":6969"
-)
+type config struct {
+	addr string
+	staticDir string
+}
 
 func main() {
-
+	
+	//flags
+	var con config
+	flag.StringVar(&con.addr,"addr", ":6969", "Network address")
+	flag.StringVar(&con.staticDir,"static-dir", "./ui/static", "Static Directory Path")
+	flag.Parse()
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(http.Dir(con.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/life/view", view)
 	mux.HandleFunc("/life/create", create)
 
-	log.Println("Starting server on " + port)
-	err := http.ListenAndServe(port, mux)
+	log.Println("Starting server on " + con.addr)
+	err := http.ListenAndServe(con.addr, mux)
 	log.Fatal(err)
 }
